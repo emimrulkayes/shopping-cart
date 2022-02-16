2bb5bd3aad8ff82d7e7165ebe72685b23d1c7927
MD. IMRUL KAYES <66476720+emimrulkayes@users.noreply.github.com>
2022-02-17 03:52:57 +0600

    added increase and decrease

--- app.js
+++ app.js
@@ -1,1 +1,29 @@
-console.log('I am in a separate file')
+function updateProductNumber(products, price, isIncreasing) {
+    const productInput = document.getElementById(products + '-counter');
+    let productNumber = productInput.value;
+
+    if (isIncreasing == true) {
+        productNumber = parseInt(productNumber) + 1;
+    } else if (productNumber > 0) {
+        productNumber = parseInt(productNumber) - 1;
+    }
+    productInput.value = productNumber;
+    // Update Total 
+    const productTotal = document.getElementById(products + '-total');
+    productTotal.innerText = productNumber * price;
+};
+
+// Handle increase decrease events 
+document.getElementById('case-plus').addEventListener('click', function() {
+    updateProductNumber('case', 1219, true);
+});
+document.getElementById('case-minus').addEventListener('click', function() {
+    updateProductNumber('case', 1219, false);
+});
+// Handle phone increase decrease events 
+document.getElementById('phone-plus').addEventListener('click', function() {
+    updateProductNumber('phone', 59, true);
+})
+document.getElementById('phone-minus').addEventListener('click', function() {
+    updateProductNumber('phone', 59, false);
+})
